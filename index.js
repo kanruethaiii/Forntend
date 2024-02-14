2e5530ecb0f99b596b311e03b2077b5d4e781bfd
kanruethaiii <s6406021612014@email.kmutnb.ac.th>
2024-02-14 10:38:26 +0700

    up

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const e = require('express');
 // Base URL for the API
 // const base_url = "https;//api.example.com"
 
-//const base_url = "http://localhost:3000";
-const base_url = "http://node57081-budsayamas-noderest.proen.app.ruk-com.cloud";
+const base_url = "http://localhost:3000";
+//const base_url = "http://node57081-budsayamas-noderest.proen.app.ruk-com.cloud";
 
 
 // Set the template engine
